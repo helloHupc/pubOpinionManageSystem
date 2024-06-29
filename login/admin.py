6046f05207959b3bf4a28292762505cda1074ac2
hupengchen <985708024@qq.com>
2024-06-30 00:09:25 +0800

    add api url and userprofile

--- login/admin.py
+++ login/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile, ConfirmString
 
-# Register your models here.
 
-from . import models
+# UserAdmin继承自ModelAdmin
+class UserProfileAdmin(UserAdmin):
+    fieldsets = (None, {
+        'fields': (
+            'username', 'password', 'email', 'roles', 'sex', 'is_superuser', 'is_staff',
+            'is_active')}),
+    readonly_fields = ('password',)
+    list_per_page = 10
 
-admin.site.register(models.User)
-admin.site.register(models.ConfirmString)
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(ConfirmString)
