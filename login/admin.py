9ba5ddc8943188c5c8f964880f8a0f472564bd90
hupengchen <985708024@qq.com>
2024-06-30 17:37:54 +0800

    add simpleui

--- login/admin.py
+++ login/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import UserProfile, ConfirmString
 
+admin.site.site_header = '舆情管理后台'  # 设置header
+admin.site.site_title = '舆情管理后台'   # 设置title
+admin.site.index_title = '舆情管理后台'
+
 
 # UserAdmin继承自ModelAdmin
 class UserProfileAdmin(UserAdmin):
